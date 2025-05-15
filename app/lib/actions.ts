d3be8b466dde05c7224f62d4926f3b073245e9f1
Vishnu D <vishnu.d.t.2004@gmail.com>
2025-05-15 20:00:15 +0530

    Chapter 13 (Handling Errors): handle errors with JS's try/catch in server actions (note: Next.js skips code after try/catch if an error is caught); use special files - error.tsx (client component, special props: error, reset; catch-all for unexpected errors and shows fallback UI), not-found.tsx (404 page, shown by calling the notFound method), and the notFound method from next/navigation (handles missing resources or pages; takes precedence over error.tsx); implemented error fallback UI for /dashboard/invoices (including its children) and 404 page for /dashboard/invoices/[id]/edit

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -33,10 +33,14 @@ export async function createInvoice(formData: FormData) {
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split("T")[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
@@ -52,17 +56,28 @@ export async function updateInvoice(id: string, formData: FormData) {
   });
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
+  try {
+    await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
   `;
+  } catch (error) {
+    console.error(error);
+  }
 
   revalidatePath("/dashboard/invoices");
   redirect("/dashboard/invoices");
 }
 
 export async function deleteInvoice(id: string) {
-  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  // throw new Error("Failed to Delete Invoice"); // manually simulated error
+
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    console.error(error);
+  }
+
   revalidatePath("/dashboard/invoices");
 }
