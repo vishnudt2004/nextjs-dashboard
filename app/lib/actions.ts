03dffbf3d082282a400c52a4a06d47824a80b33d
Vishnu D <vishnu.d.t.2004@gmail.com>
2025-05-19 00:46:30 +0530

    Chapter 15 (Adding Authentication): authentication (username, password, verification code, 2FA, authenticator, OAuth), authorization (determines what a user can do or access); NextAuth.js (lib: next-auth(@beta), manual implementation is time-consuming and error-prone, so next-auth simplifies the process and handles authentication complexities like session management, sign-in/sign-out, etc.); security (encryption with secret keys, password hashing); Next.js middleware (special file at root: middleware.ts - runs before every request completes; receives props: auth (user session), request (incoming request)); implemented authentication logic (flow: login (/login/page.tsx) → server action (/lib/actions.ts authenticate) → call signIn (from Auth.js) → auth config (auth.config.ts, auth.ts) → session saved in cookie → redirect to callback URL (/dashboard); logout (/dashboard/layout.tsx) → server action (/ui/dashboard/sidenav.tsx) → call signOut (from Auth.js) → middleware (middleware.ts) catches & redirects to login (/login, handled by Auth.js))

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import postgres from "postgres";
+import { AuthError } from "next-auth";
+import { signIn } from "@/auth";
 
 const sql = postgres(process.env.POSTGRES_URL!); // omitted: ssl option
 
@@ -116,4 +118,23 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData
+) {
+  try {
+    await signIn("credentials", formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return "Invalid credentials.";
+        default:
+          return "Something went wrong.";
+      }
+    }
+    throw error;
+  }
 }
