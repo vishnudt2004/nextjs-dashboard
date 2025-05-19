2cecd93d3cd097f0fe187e2975d33c5a0f8115bc
Vishnu D <vishnu.d.t.2004@gmail.com>
2025-05-19 22:18:33 +0530

    Post-course addition (Custom commits): implemented missing /dashboard/customers page from course (copied from final example - https://github.com/vercel/next-learn/blob/main/dashboard/final-example/app/dashboard/customers/page.tsx)

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,25 @@
 import { Metadata } from "next";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+import CustomersTable from "@/app/ui/customers/table";
 
 export const metadata: Metadata = {
   title: "Customers",
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <main>
+      <CustomersTable customers={customers} />
+    </main>
+  );
 }
